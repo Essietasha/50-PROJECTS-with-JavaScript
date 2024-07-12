1250d4e5d72948bd1c908e709b80a3661d175a3c
Essietasha <163308907+Essietasha@users.noreply.github.com>
2024-07-12 11:16:28 +0300

    Update script.js

--- Projects/pokeman/script.js
+++ Projects/pokeman/script.js
@@ -5,7 +5,7 @@ fetchImages();
 async function fetchImages() {
     try {
 
-        const searchitem = document.getElementById('inputEl').value;
+        const searchitem = document.getElementById('inputEl').value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchitem}`);
         
         if (!response.ok) {
@@ -13,14 +13,18 @@ async function fetchImages() {
         }
 
         const data = await response.json();
+        console.log(searchitem);
         console.log(data);
         const pokemanDisplay = data.sprites.front_default;
         const imgEl = document.getElementById('imgElement');
+        const pokemanName = document.querySelector('.pokemanName');
 
         imgEl.src = pokemanDisplay;
         imgEl.style.display = 'block';
+        pokemanName.innerHTML = data.name;
 
-    } catch (error) {
-        console.error('Error fetching data:', error);
+    }
+    catch(error) {
+        console.error(error);
     }
 }
